538630494e19bd9175ee41a4b59ac57d6802e71a
roushansolanki <roushansolanki@gmail.com>
2026-03-12 01:03:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,24 +3,20 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-// Frontend files ko serve karne ke liye
 app.use(express.static('public'));
 
 let broadcaster;
 
 io.on('connection', (socket) => {
-  // Camera device connect hone par
   socket.on('broadcaster', () => {
     broadcaster = socket.id;
     socket.broadcast.emit('broadcaster');
   });
 
-  // Viewer device connect hone par
   socket.on('watcher', () => {
     socket.to(broadcaster).emit('watcher', socket.id);
   });
 
-  // WebRTC Signaling data exchange
   socket.on('offer', (id, message) => {
     socket.to(id).emit('offer', socket.id, message);
   });
@@ -30,6 +26,12 @@ io.on('connection', (socket) => {
   socket.on('candidate', (id, message) => {
     socket.to(id).emit('candidate', socket.id, message);
   });
+  
+  // NAYA FEATURE: Remote Commands Relay
+  socket.on('remoteCommand', (command) => {
+    socket.to(broadcaster).emit('remoteCommand', command);
+  });
+
   socket.on('disconnect', () => {
     socket.broadcast.emit('disconnectPeer', socket.id);
   });
