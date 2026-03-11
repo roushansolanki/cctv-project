4c6be7d040be1f25ce0e9444ed8b902115724187
roushansolanki <roushansolanki@gmail.com>
2026-03-12 02:32:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,24 +5,52 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
-let broadcaster;
+let broadcasters = {}; // Store multiple cameras
 
 io.on('connection', (socket) => {
-  socket.on('broadcaster', () => { broadcaster = socket.id; socket.broadcast.emit('broadcaster'); });
-  socket.on('watcher', () => { socket.to(broadcaster).emit('watcher', socket.id); });
-  
-  socket.on('offer', (id, message) => { socket.to(id).emit('offer', socket.id, message); });
-  socket.on('answer', (id, message) => { socket.to(id).emit('answer', socket.id, message); });
-  socket.on('candidate', (id, message) => { socket.to(id).emit('candidate', socket.id, message); });
-  
-  // Remote Commands & Info Relay
-  socket.on('remoteCommand', (command) => { socket.to(broadcaster).emit('remoteCommand', command); });
-  socket.on('batteryInfo', (info) => { socket.broadcast.emit('batteryInfo', info); });
-  socket.on('gpsInfo', (info) => { socket.broadcast.emit('gpsInfo', info); }); // NAYA: GPS
-  socket.on('viewerMessage', (msg) => { socket.broadcast.emit('viewerMessage', msg); });
-  socket.on('voiceCommand', (audioData) => { socket.to(broadcaster).emit('voiceCommand', audioData); });
+  // Jab koi phone camera chalu kare
+  socket.on('broadcaster', (id) => {
+    broadcasters[socket.id] = { id: id, socketId: socket.id };
+    socket.broadcast.emit('broadcaster', socket.id, id);
+  });
 
-  socket.on('disconnect', () => { socket.broadcast.emit('disconnectPeer', socket.id); });
+  // Viewer ko saare available cameras batana
+  socket.on('getBroadcasters', () => {
+    Object.values(broadcasters).forEach(b => {
+        socket.emit('broadcaster', b.socketId, b.id);
+    });
+  });
+
+  socket.on('watcher', (broadcasterId) => {
+    socket.to(broadcasterId).emit('watcher', socket.id);
+  });
+
+  socket.on('offer', (id, message) => {
+    socket.to(id).emit('offer', socket.id, message);
+  });
+  socket.on('answer', (id, message) => {
+    socket.to(id).emit('answer', socket.id, message);
+  });
+  socket.on('candidate', (id, message) => {
+    socket.to(id).emit('candidate', socket.id, message);
+  });
+
+  // Control commands targeted to specific phone
+  socket.on('remoteCommand', (targetId, command) => {
+    socket.to(targetId).emit('remoteCommand', command);
+  });
+
+  socket.on('voiceCommand', (targetId, audioData) => {
+    socket.to(targetId).emit('voiceCommand', audioData);
+  });
+
+  socket.on('batteryInfo', (info) => { socket.broadcast.emit('batteryInfo', socket.id, info); });
+  socket.on('gpsInfo', (info) => { socket.broadcast.emit('gpsInfo', socket.id, info); });
+
+  socket.on('disconnect', () => {
+    delete broadcasters[socket.id];
+    socket.broadcast.emit('disconnectPeer', socket.id);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
